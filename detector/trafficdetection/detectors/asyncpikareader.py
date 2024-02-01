d789594432643172a76fe34cf408c00b306f80ed
Alexander Foley <alexandergfoley@gmail.com>
2024-02-01 11:19:16 -0700

    batching things out

--- detector/trafficdetection/detectors/asyncpikareader.py
+++ detector/trafficdetection/detectors/asyncpikareader.py
@@ -3,6 +3,7 @@ import logging
 import time
 import pika
 import cv2
+from deepface import DeepFace
 
 from pika.adapters.asyncio_connection import AsyncioConnection
 from pika.exchange_type import ExchangeType
@@ -28,7 +29,7 @@ class AsyncPikaReader(object):
     EXCHANGE = "videostreams"
     EXCHANGE_TYPE = ExchangeType.topic
     ROUTING_KEY = "streams"
-    QUEUE = ""  # makes randomly named queue
+    QUEUE = "batchedstreams"  # makes randomly named queue
 
     def __init__(self, amqp_url):
         """Create a new instance of the consumer class, passing in the AMQP
@@ -418,10 +419,79 @@ class AsyncPikaReader(object):
             ret, frame = cap.read()
             id += 1
             if ret == True:
-                LOGGER.info(id)
+                self.process(frame)
             # Break the loop
             else:
                 break
+
+    def process(self, frame, add_labels=True):
+        detector_backend="retinaface"
+        actions=["age", "gender", "race", "emotion"]
+        res = DeepFace.analyze(
+            frame,
+            enforce_detection=False,
+            detector_backend=detector_backend,
+            actions=actions,
+            silent=True,
+        )
+
+
+        if len(res) > 0:
+            curr_y = 30
+            curr_x = 0
+            (jump_x, jump_y), _ = cv2.getTextSize(
+                "Emotion: Disgust", cv2.FONT_HERSHEY_SIMPLEX, 2, 2
+            )
+
+            for idx, r in enumerate(res):
+                age = r["age"]
+                gender = r["dominant_gender"]
+                race = r["dominant_race"]
+                emotion = r["dominant_emotion"]
+                region = r["region"]
+                x, y, w, h = region["x"], region["y"], region["w"], region["h"]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
+                cv2.putText(
+                    frame,
+                    f"ID: {idx}",
+                    (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    (0, 0, 0),
+                    2,
+                )
+
+                if add_labels:
+                    labels = [
+                        f"ID: {idx}",
+                        f"Age: {age}",
+                        f"Gender: {gender}",
+                        f"Race: {race}",
+                        f"Emotion: {emotion}",
+                    ]
+
+                    cv2.rectangle(
+                        frame,
+                        (curr_x, curr_y - jump_y),
+                        (curr_x + jump_x, curr_y + len(labels) * jump_y),
+                        (255, 255, 255),
+                        -1,
+                    )
+
+                    for label in labels:
+                        cv2.putText(
+                            frame,
+                            label,
+                            (curr_x, curr_y),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            2,
+                            (0, 0, 0),
+                            2,
+                        )
+                        curr_y += jump_y
+                    curr_x += jump_x
+                    curr_y = 30
+        return frame, res
 
 
 class ReconnectingAsyncPikaReader(object):
