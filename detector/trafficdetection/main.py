52654f4957245f5b562a435b143c24f5cadca57b
afoley587 <54959686+afoley587@users.noreply.github.com>
2024-02-02 09:56:32 -0700

    feat: python-000 Migrating to asyncio and cleanup (#5)

--- detector/trafficdetection/main.py
+++ detector/trafficdetection/main.py
@@ -1,53 +1,75 @@
+# import os
+# import utils
+# from loguru import logger
+# from criteria.trafficking import is_possible_trafficking
+
+# import asyncio
+import signal
+import sys
 import os
-import utils
-from loguru import logger
-from criteria.trafficking import is_possible_trafficking
+
+from readers.asyncpikareader import ReconnectingAsyncPikaReader
 
 
-def main():
-    args = utils.parse_args()
-
-    if args.rabbit is not None:
-        # Streams From A camera
-        utils.analyze_rabbit(
-            params=args.rabbit,
-            criterias=[is_possible_trafficking],
-            show=args.show_results,
-            save=args.save_results,
-        )
-    if args.video_device is not None:
-        # Streams From A camera
-        utils.analyze_video(
-            device=args.video_device,
-            criterias=[is_possible_trafficking],
-            show=args.show_results,
-            save=args.save_results,
-        )
-    if args.video_file is not None:
-        # Streams From A camera
-        utils.analyze_video(
-            device=args.video_file,
-            criterias=[is_possible_trafficking],
-            show=args.show_results,
-            save=args.save_results,
-        )
-
-    if args.images_directory is not None:
-        if not os.path.exists(args.images_directory):
-            logger.error(f"{args.images_directory} Does Not Exist")
-            raise FileNotFoundError
-
-        # Spot check images
-        for root, subdir, files in os.walk(args.images_directory):
-            for file in files:
-                path = os.path.join(root, file)
-                utils.analyze_image(
-                    path,
-                    criterias=[is_possible_trafficking],
-                    show=args.show_results,
-                    save=args.save_results,
-                )
+def start():
+    amqp_url = "amqp://guest:guest@rabbitmq:5672/%2F"
+    consumer = ReconnectingAsyncPikaReader(amqp_url)
+    consumer.run()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        start()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(signal.SIGINT)
+        except SystemExit:
+            os._exit(signal.SIGINT)
+
+# def main():
+#     args = utils.parse_args()
+
+#     if args.rabbit is not None:
+#         # Streams From A camera
+#         utils.analyze_rabbit(
+#             params=args.rabbit,
+#             criterias=[is_possible_trafficking],
+#             show=args.show_results,
+#             save=args.save_results,
+#         )
+#     if args.video_device is not None:
+#         # Streams From A camera
+#         utils.analyze_video(
+#             device=args.video_device,
+#             criterias=[is_possible_trafficking],
+#             show=args.show_results,
+#             save=args.save_results,
+#         )
+#     if args.video_file is not None:
+#         # Streams From A camera
+#         utils.analyze_video(
+#             device=args.video_file,
+#             criterias=[is_possible_trafficking],
+#             show=args.show_results,
+#             save=args.save_results,
+#         )
+
+#     if args.images_directory is not None:
+#         if not os.path.exists(args.images_directory):
+#             logger.error(f"{args.images_directory} Does Not Exist")
+#             raise FileNotFoundError
+
+#         # Spot check images
+#         for root, subdir, files in os.walk(args.images_directory):
+#             for file in files:
+#                 path = os.path.join(root, file)
+#                 utils.analyze_image(
+#                     path,
+#                     criterias=[is_possible_trafficking],
+#                     show=args.show_results,
+#                     save=args.save_results,
+#                 )
+
+
+# if __name__ == "__main__":
+#     main()
